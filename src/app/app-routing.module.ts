29fcb7f486078e0ef7bb08842df74a162e06ce8e
Shagun998 <ssharma998855@gmail.com>
2022-03-09 02:24:34 -0800

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
       path: 'carRental', component:  CubixCabComponent
      },
      {
-      path: '**', pathMatch: 'full', 
+      path: 'not-found', pathMatch: 'full', 
       component:NotFoundComponent
      },
    
